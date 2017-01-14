66b0ab2adbea8923e030adc2338838767c939a36
Michael Matias <michael@michaelmatias.com>
2017-01-14 13:31:03 +0200

    removed top folder heirarchy, reorganizing files

--- hatulia-application/src/app/config/AppSettings.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by michaelmatias on 1/6/17.
- */
-export class AppSettings {
-  public static get SUBMIT_ORDER_URL(): string { return this.SERVER + 'submitOrder/' }
-  public static get GET_MENU_DETAILS(): string { return this.SERVER + 'menuDetails/' }
-  public static get SERVER(): string { return 'http://localhost:4338/'; }
-}
